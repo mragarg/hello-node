d08f96e28b9898ee1067164f66c74b68d21c938b
Ashish Garg <mragarg@gmail.com>
2019-03-26 09:29:21 -0400

    Testing async and await

--- examples.js
+++ examples.js
@@ -6,6 +6,24 @@ function getUserById(theId){
 }
 // getUserById(2).then(console.log);
 
+
+//////////////////////////////////// ASYNC / AWAIT TESTING ////////////////////////////////////
+async function getUserById2(theId){
+    return await db.any(`select * from users where id=${theId}`)
+}
+
+// Async functions can use the keyword "await"
+async function main(){
+	// Async + Await
+	const user3 = await getUserById2(3);	// "await" waits for promises 
+											// it's like an implicit ".then"
+	console.log(user3);
+}
+
+main();
+
+////////////////////////////////////////////////////////////////////////////////////////////////
+
 // 1A. get only a few fields for public version
 function getUserPublic(theId){
     return db.any(`select first_name from users where id=${theId}`)
@@ -20,9 +38,19 @@ function getUserPrivate(theId){
 
 // 2. get all favorites for a user by id
 function getUserFavorites(theId){
-    return db.any(`select user_id, restaurant_id,first_name from favorites f
+    return db.any(`select user_id, restaurant_id, first_name from favorites f
 	inner join users u on
 		f.user_id = u.id
 	where f.user_id=${theId};`)
 }
-getUserFavorites(1).then(console.log);
+// getUserFavorites(1).then(console.log);
+
+// 3. get all reviews written by that user by id
+function getUserReviews(theId){
+    return db.any(`select score, content, user_id, restaurant_id, first_name from reviews r
+	inner join users u on
+		r.user_id = u.id
+	where r.user_id=${theId}`)
+}
+// getUserReviews(2).then(console.log);
+
