f389e82717fd3337ecb298e2a5e048c64ea5f1d5
Ashish Garg <mragarg@gmail.com>
2019-03-26 11:18:55 -0400

    Adds test case where .any is changed to .one

--- models/user.js
+++ models/user.js
@@ -4,13 +4,23 @@ const db = require('./conn');
 // Need a User...object? thing? something?
 class User {
 
+    constructor()
+    
     // "static" means that the function is something 
     // the class can do, but an instance cannot.
     static getById(id) {
-        return db.any(`select * from users where id=${id}`);
+        // .any always returns an array
+        // Instead, we'll use .one
+        // return db.any(`select * from users where id=${id}`);
+        return db.one(`select * from users where id=${id}`);
     }
 
 }
+
+User.getById(3)
+    .then((user) => {
+        console.log(user);
+    })
 
 // Export my User model
 module.exports = User;
