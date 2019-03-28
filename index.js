08ea3b47a3d3458ca53b0b6c8093cfd8e630383e
Ashish Garg <mragarg@gmail.com>
2019-03-28 10:38:08 -0400

    Adds nodemon

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
-const sendHelpTo= require('./helper.js');
+const http = require('http');
 
-const message = sendHelpTo("Dora");
-console.log("Hello, Node");
-console.log(message);
+const hostname = '127.0.0.1';
+const port = 3000;
 
-const cowsay = require('cowsay');
-console.log(cowsay.say({
-	text : "I'm a moooodule",
-	e : "oO",
-	T : "U "
-}));
+// "Helper function"  === "middleware"
+// a.k.a. "Request handler"
+const server = http.createServer((req, res) => {
+	console.log(req.url);
+
+	res.statusCode = 200;
+	res.setHeader('Content-Type', 'text/plain');
+
+	res.end('Hello you...!');
+});
+
+server.listen(port, hostname, () => {
+	console.log(`Server is running at http://${hostname}:${port}`);
+});
